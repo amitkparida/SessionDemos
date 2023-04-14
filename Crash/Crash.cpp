af09d3a4dcd11854d86475e5edb206d6ab5095fe
amitkparida <aamit.parida@outlook.com>
2023-04-14 16:51:22 +0530

    .

--- Crash/Crash.cpp
+++ Crash/Crash.cpp
@@ -1,3 +1,23 @@
+
+
+// demo programs for stack and heap corruptions
+// See vganga session 5 for heap corruption debugging video with !heap, !uiext.heap commands
+
+
+// Note:
+// In case of stack buffer overrun, the hackers can take control over the Return Address (by placing the address of their malicious function in place of the Return Address in stack)
+
+// In case of heap buffer overrun, the hackers can take control over the Virtual Table of the dynamically created Class object if you have a buffer and a vtable in the object.
+// (by placing address of their malicious function in the virtual Table in place of the virtual function addresses)
+
+
+// In order to prevent stack buffer overrun, VC++ compiler uses /GS(Guard Stack) switch by default. 
+// Properties->C/C++->Code Generation->Security Check->Enable Security Check(/GS)
+// /GS is on by default.If you expect your application to have no security exposure, use /GS-.
+// The /GS compiler option does not protect against all buffer overrun security attacks. For example, if you have a buffer and a vtable in an object, a buffer overrun could corrupt the vtable.
+// Even if you use / GS, always try to write secure code that has no buffer overruns.
+
+
 #define _CRT_SECURE_NO_WARNINGS
 //#pragma warning(disable:4996)
 
@@ -5,16 +25,16 @@
 #include <windows.h>
 using namespace std;
 
-
-//demo program for heap corruption
-//See vganga session 5 for heap corruption debugging video with !heap, !uiext.heap commands
-
 #define SZ_MAX_LEN 5
 
 void CorruptStack()
 {
-	char str1[5];
-	strcpy(str1, "amitkumarparida"); // It will corrupt the pushed EBP and Return Address (EIP)
+	char buffer[5];
+	strcpy(buffer, "amitkumarparida"); // It will corrupt the pushed EBP and Return Address (EIP)
+
+	// use a secure CRT function to help prevent buffer overruns
+	// truncate string to fit into destination buffer
+	// strncpy_s(buffer, _countof(buffer), "amitkumarparida", _TRUNCATE);
 }
 
 void fun1(char* pStr)
@@ -28,13 +48,13 @@ void fun1(char* pStr)
 }
 void CorruptHeap()
 {
-	char* str1 = new char[SZ_MAX_LEN];
-	strcpy(str1, "amitkumar"); //Heap Buffer overrun and hence corruption
+	char* buffer = new char[SZ_MAX_LEN];
+	strcpy(buffer, "amitkumar"); //Heap Buffer overrun and hence corruption
 
-	fun1(str1);
+	fun1(buffer);
 
-	cout << str1 << endl;
-	delete[] str1;
+	cout << buffer << endl;
+	delete[] buffer;
 }
 
 int main()
@@ -45,11 +65,4 @@ int main()
 
 
 
-//demo program for stack corruption
 
-
-
-//Note:
-//In case of stack buffer overrun, the hackers can take control over the Return Address(by placing the address of their malicious function in place of the Return Address in stack)
-
-//In case of heap buffer overrun, the hackers can take control over the Virtual Table of the dynamically created Class object. (by placing address of their malicious function in the virtual Table)
--- Crash/StackOverrun.cpp
+++ Crash/StackOverrun.cpp
@@ -1,32 +1,74 @@
 //Stack Overruns
 
-#define _CRT_SECURE_NO_WARNINGS
-//#pragma warning(disable:4996)
+// Note:
+// In case of stack buffer overrun, the hackers can take control over the Return Address (by placing the address of their malicious function in place of the Return Address in stack)
 
-#include <windows.h>
-#include <stdio.h>
-#define MAX_CONN_LEN 5
+// In order to prevent stack buffer overrun, VC++ compiler uses /GS(Guard Stack) switch by default. 
+// Properties->C/C++->Code Generation->Security Check->Enable Security Check(/GS)
+// /GS is on by default.If you expect your application to have no security exposure, use /GS-.
+// The /GS compiler option does not protect against all buffer overrun security attacks. For example, if you have a buffer and a vtable in an object, a buffer overrun could corrupt the vtable.
+// Even if you use / GS, always try to write secure code that has no buffer overruns.
 
-void HelperFunction(char* pszConnectionString){
-	char szCopy[MAX_CONN_LEN];
-	strcpy(szCopy, pszConnectionString);
 
-	//
-	// ...
-	// Establish connection
-	// ...
-	//
+
+//#define _CRT_SECURE_NO_WARNINGS
+////#pragma warning(disable:4996)
+//
+//#include <windows.h>
+//#include <stdio.h>
+//#define MAX_CONN_LEN 5
+//
+//void HelperFunction(char* pszConnectionString){
+//	char szCopy[MAX_CONN_LEN];
+//	strcpy(szCopy, pszConnectionString);
+// 
+// 	// use a secure CRT function to help prevent buffer overruns
+//	// truncate string to fit into destination buffer
+//	// strncpy_s(szCopy, _countof(szCopy), pszConnectionString, _TRUNCATE);
+//
+//	//
+//	// ...
+//	// Establish connection
+//	// ...
+//	//
+//}
+//
+//int main(){
+//	char szStr[MAX_CONN_LEN];
+//	
+//	printf("Please specify connection string on the command line:\n");
+//	scanf("%[^\n]", szStr);
+//	HelperFunction(szStr);
+//	//printf("Connection to %s established\n", szStr);
+//
+//	return 0;
+//}
+
+
+
+
+// https://www.youtube.com/watch?v=gqixbRLRJf0
+// Below program will crash with stack corruption error by default as /GS is enabled.
+// 
+// If you disable /GS (and build it in Release preferably), then below program will get into infinite loop. When x becomes 100 in the loop, crashArray[100] buffer overflows. 
+// In stack, int x memory is allicated next to the buffer. So, int x memory will be overwritten (as 'a' or 97) when crashArray buffer overflows.
+// This will cause infinite loop.
+
+#include <iostream>
+
+void func()
+{
+	std::cout << "going to loop forever" << std::endl;
+
+	char crashArray[100];
+	for (int x = 0; x <= 100; ++x) {
+		*(crashArray + x) = 'a';
+	}
+
+	std::cout << "should not reach here" << std::endl;
 }
 
-int main(){
-	char szStr[MAX_CONN_LEN];
-	
-	printf("Please specify connection string on the command line:\n");
-	scanf("%[^\n]", szStr);
-	HelperFunction(szStr);
-	//printf("Connection to %s established\n", szStr);
-
-	return 0;
+int main()
+{
+	func();
 }
-
-
